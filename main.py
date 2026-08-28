bb8db706b02c0e080906a921a7578da5caf5688f
Ale <aleiannibelli@gmail.com>
2026-08-28 10:46:59 -0300

    Funcion para Cargar Programa terminada

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 from funciones import ingresar_n, tipo_jugador, tipo_equipo, armar_tabla, tabla_equipos, ordenar_tabla, formato_jugadores, formato_equipo
 from funciones import agregar_puntos, sumar_puntos, elegir_opcion
-from persistencia import archivo_existe, guardar_datos, guardar_archivo
+from persistencia import archivo_existe, guardar_datos, guardar_archivo, mostrar_archivos, cargar_archivo, eliminar_archivo
 
 print("Iniciando Programa")
 print("--")
@@ -50,25 +50,67 @@ if opcion == 1:
     
 elif opcion == 2:
     print("")
-    #Acá deberíamos poder mostrar los nombres de los archivos disponibles
+    archivos = mostrar_archivos()
+    for i in archivos:
+        print(i)
+    
     nombre = input("Ingrese el nombre del campeonato que desea reanudar: ").strip()
-    existe = archivo_existe(nombre + ".json")
+    existe = archivo_existe(nombre)
     while not existe:
         print("--")
         print(f"Error: no existe ningún campeonato con el nombre {nombre}")
+        print("")
+        for i in archivos:
+            print(i)
+        print("")
         nombre = input("Ingrese el nombre del campeonato que desea reanudar: ").strip()
-        existe = archivo_existe(nombre + ".json")
+        existe = archivo_existe(nombre)
+
+    datos = cargar_archivo(nombre)
+    tipo_j = datos["configuracion"]["categoria_jugador"]
+    tipo_e = datos["configuracion"]["categoria_equipo"]
+    cant = datos["configuracion"]["cantidad_equipos"]
+    tabla_ind = datos["tabla_jugadores"]
+    tabla_gru = datos["tabla_equipos"]
+
+    print("")
+    formato_jugadores(tabla_ind, tipo_j, tipo_e)
+    formato_equipo(tabla_gru, tipo_e)
+
+    sigue = agregar_puntos()
+    while sigue:
+        sumar_puntos(tabla_ind, tipo_j)
+        sigue = agregar_puntos()
+
+    ordenar_tabla(tabla_ind)
+    tabla_gru = tabla_equipos(tabla_ind)
+    config = {
+        "categoria_jugador": tipo_j,
+        "categoria_equipo": tipo_e,
+        "cantidad_equipos": cant
+        }
+    datos = guardar_datos(config, tabla_ind, tabla_gru)
+
+    guardar_archivo(nombre, datos)
+
 
 elif opcion == 3:
     print("")
-    #Acá deberíamos poder mostrar los nombres de los archivos disponibles
+    archivos = mostrar_archivos()
+    for i in archivos:
+        print(i)
+
     nombre = input("Ingrese el nombre del campeonato que desea eliminar: ").strip()
-    existe = archivo_existe(nombre + ".json")
+    existe = archivo_existe(nombre)
     while not existe:
         print("--")
         print(f"Error: no existe ningún campeonato con el nombre {nombre}")
         nombre = input("Ingrese el nombre del campeonato que desea eliminar: ").strip()
-        existe = archivo_existe(nombre + ".json")
+        existe = archivo_existe(nombre)
+
+
+
+    
 else:
     print("")
     print("No se realizaron acciones")
--- persistencia.py
+++ persistencia.py
@@ -18,3 +18,21 @@ def guardar_datos(configuracion, tabla_jugadores, tabla_equipos):
 def guardar_archivo(nombre, datos):
     with open(nombre + ".json", "w") as archivo:
         json.dump(datos, archivo)
+
+def cargar_archivo(nombre):
+    with open(nombre + ".json", "r") as archivo:
+        datos = json.load(archivo)
+
+    return datos
+
+def eliminar_archivo(nombre):
+    os.remove(nombre + ".json")
+
+def mostrar_archivos():
+    archivos_json = []
+    for i in os.listdir("."):
+        if i.endswith(".json"):
+            limpio, extension = os.path.splitext(i)
+            archivos_json.append(limpio)
+
+    return archivos_json
